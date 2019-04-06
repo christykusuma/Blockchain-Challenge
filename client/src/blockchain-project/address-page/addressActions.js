b22f565d8d60e1b2c5f8be171af55a4ecef486b4
christy.kusuma <>
2019-04-06 00:27:29 -0400

    basic setup frontend/backend, and successfully fetches data from blockchain api

--- client/src/blockchain-project/address-page/addressActions.js
+++ client/src/blockchain-project/address-page/addressActions.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
-import { data } from './data'
 
 export const fetchAddress = () => async (dispatch) => {
-
     const res = await axios.get(`/api/fetch_data`);
     console.log('dummy data', res.data);
-
 };
-
-// to connect to the backend
-// const res = await axios.post('/api/fetch_address')
-// console.log('christy results', res)
--- routes/blockchainRoutes.js
+++ routes/blockchainRoutes.js
@@ -1,16 +1,6 @@
 const axios = require('axios');
 
 module.exports = (app) => {
-/* 
-Although we are using a front end that normally does http calls, You can still
- use axios on the backend. The reason why is because currently on the front
- end, the 'Access-Control-Allow-Origins' (ACAO) isn't accessible since we are
- grabbing data from an outside source instead of fetching from our web server.
- The browser doesn't handle ACAO responses, the web server does. To get around
- it we implement the middleware in our web server since it contains the proper
- headers to make requests accross other http/https servers. SImply use an axios call
- and the required data from the url will get passed back
-*/
     app.get('/api/fetch_data', (req, res, next) => {
 
         axios
